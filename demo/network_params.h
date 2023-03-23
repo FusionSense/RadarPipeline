a84c836be26760bbabe596290147876e538beffa
Owen Convery <convery.owen@gmail.com>
2023-03-22 23:25:02 -0700

    Added new CMakeLists.txt files

--- demo/network_params.h
+++ demo/network_params.h
@@ -1,9 +1,11 @@
 ///////// Server details /////////
 
-#define ROUTER_IP
-#define GATEWAY_IP
-#define SERVER_TCP_PORT
-#define SERVER_SSH_PORT 491
+#define LOCALHOST "127.0.0.1"
+
+#define ROUTER_IP "192.168.1.1"
+#define GATEWAY_IP ""
+#define SERVER_TCP_PORT 
+#define SERVER_SSH_PORT "491"
 
 ////////// Data details //////////
 
@@ -12,21 +14,21 @@
 
 ///////// Node A details /////////
 
-#define NODE_A_IP
+#define NODE_A_IP "192.168.1.101"
 #define NODE_A_TCP_PORT
-#define NODE_A_SSH_PORT 491
+#define NODE_A_SSH_PORT "491"
 
 ///////// Node B details /////////
 
-#define NODE_B_IP
+#define NODE_B_IP "192.168.1.102"
 #define NODE_B_TCP_PORT
-#define NODE_B_SSH_PORT 491
+#define NODE_B_SSH_PORT "491"
 
 ///////// Node C details /////////
 
-#define NODE_B_IP
-#define NODE_B_TCP_PORT
-#define NODE_B_SSH_PORT 491
+#define NODE_B_IP "192.168.1.103"
+#define NODE_B_TCP_PORT 
+#define NODE_B_SSH_PORT "491"
 
 // Add logic to assign params based on a another config file
 
