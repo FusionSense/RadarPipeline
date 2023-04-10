c8e5a0f2738d34ebc25ba4b9314f9543008a4038
Owen Convery <convery.owen@gmail.com>
2023-04-09 19:42:41 -0700

    Added IP

--- demo/speed_test/client.cpp
+++ demo/speed_test/client.cpp
@@ -44,7 +44,7 @@ int main()
  
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_addr.s_addr = inet_addr(IP);
     servaddr.sin_port = htons(TCP_PORT);
  
     // connect the client socket to server socket
