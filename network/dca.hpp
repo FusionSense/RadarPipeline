5fb81f2f38bb6b6fc389862791e22e3d6b01c52f
Owen Convery <convery.owen@gmail.com>
2023-04-12 11:34:57 -0700

    Added source and destination IP addresses

--- network/dca.hpp
+++ network/dca.hpp
@@ -1,5 +1,7 @@
 ////////// DCA details //////////
 
-#define DCA_IP "192.168.33.30"
-#define CONFIG_PORT 4096
-#define DATA_PORT 4098
+#define SOURCE_IP "192.168.33.180"
+#define DESTINATION_IP "192.168.33.30"
+
+#define SOURCE_PORT 4096
+#define DESTINATION_PORT 4098
