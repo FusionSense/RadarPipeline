2be547866da3ce8d71c2f7adef079e069a4a0216
Owen Convery <convery.owen@gmail.com>
2023-04-15 17:35:45 -0700

    Added better testing demo

--- test/test.cpp
+++ test/test.cpp
@@ -6,16 +6,32 @@ int main()
     uint inputframe = 0;
     uint* inputptr = &inputframe;
 
+    cout << "Upstream frame: " << inputframe << endl;
+    cout << "Address: " << inputptr << endl;
+    cout << "\n" << endl;
+
     // New block
     RadarBlock obj(8);
     obj.setInputPointer(inputptr);
 
+    int* bufferptr = obj.getBufferPointer();
+    uint* frameptr = obj.getFramePointer();
+
+    cout << "Output buffer address: " << bufferptr << endl;
+    cout << "\n" << endl;
+
+    // Multi-threading this block
     thread myThread(&RadarBlock::iteration, &obj, 42);
+
+    // Incrementing the upstream frame
     inputframe++;
 
-    // int* bufferptr = obj.getBufferPointer();
-    // uint* frameptr = obj.getFramePointer();
+    cout << "Upstream frame: " << inputframe << endl;
+    cout << "Address: " << inputptr << endl;
+    cout << "\n" << endl;
 
+    // Exit upon execution of thread
     myThread.join();
+
     return 0;
 }
